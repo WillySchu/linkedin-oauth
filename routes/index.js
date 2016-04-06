782f2fb14a8ee742dfd80d4afe354cdd18b09e6f
Will Schumacher <meintodengel@gmail.com>
2016-04-06 15:13:02 -0700

    add logout

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  console.log(req.user);
   res.render('index', { title: 'Express' });
 });
+
+router.get('/logout', (req, res, next) => {
+  req.session = null;
+  res.redirect('/');
+})
 
 module.exports = router;
